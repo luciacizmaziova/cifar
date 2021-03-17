f0d9905dd0d1e43af598a7e0f0c493effa9ec5a8
Hassan <hassanelsabbagh25@gmail.com>
2021-03-17 14:39:17 +0200

    delete-slim-ben-ali.md

--- src/lib/find-image-by-id.js
+++ src/lib/find-image-by-id.js
@@ -1,8 +1,14 @@
 export default (images, id) => {
-  const image = images.find(({ node: { parent: { absolutePath } } }) => {
-    const normalizedId = id >= 10 ? id : `0${id}`;
-    return absolutePath.includes(`/media/${normalizedId}-`);
-  });
+  const image = images.find(
+    ({
+      node: {
+        parent: { absolutePath }
+      }
+    }) => {
+      const normalizedId = id >= 10 ? id : `0${id}`;
+      return absolutePath.includes(`/media/${normalizedId}-`);
+    }
+  );
 
   // if no image was found, return the default avatar
   if (!image) {
